bc0e614218f29e6f069fa4a2f5ff813a4b77f7f3
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2024-04-02 13:59:56 +0200

    work?

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -24,7 +24,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173',
+    baseURL: process.env.GITHUB_ACTIONS
+      ? 'http://localhost:5000'
+      : 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
