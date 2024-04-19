9cb4542c1c57fcb07896f0fd949fb1675e72f11d
Rokas Kasperavicius <rokaskasperavicius.github@gmail.com>
2024-04-19 15:49:49 +0200

    changes to yml files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,4 @@ source ~/.bash_profile
 cd /minitwit/scripts
 
 docker compose -f docker-compose.yml pull
-
-# Stop all old docker containers from running to run the new one
-docker stop $(docker ps -a -q)
-docker compose -f docker-compose.yml up -d
+docker stack deploy ministack --compose-file docker-compose.prod.yml
