85fa9fa2348f987bda23fb3f2ac12e56e99c9f6c
Hopeberg <anam@itu.dk>
2024-04-27 14:06:28 +0200

    terraform swarm is working Co-authored by: ChatGPT

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,11 +78,20 @@ resource "digitalocean_droplet" "manager" {
       "echo 'export PSQL_CON_STR=${var.PSQL_CON_STR}' >> ~/.bash_profile",
       "echo 'export SECRET_COOKIE_HMAC=${var.SECRET_COOKIE_HMAC}' >> ~/.bash_profile",
       "echo 'export SECRET_COOKIE_AES=${var.SECRET_COOKIE_AES}' >> ~/.bash_profile",
+      "sudo ufw allow 2377/tcp",
+      "sudo ufw allow 7946/tcp",
+      "sudo ufw allow 7946/udp",
+      "sudo ufw allow 4789/udp",
+      "sudo ufw reload",
       "docker swarm init --advertise-addr ${self.ipv4_address_private}",
       "SWARM_TOKEN=$(docker swarm join-token -q worker)",
       "echo $SWARM_TOKEN > /tmp/swarm_token",
     ]
   }
+}
+
+locals {
+  manager_ip = data.digitalocean_droplet.manager.ipv4_address_private 
 }
 
 resource "digitalocean_droplet" "worker-1" {
@@ -106,39 +115,16 @@ resource "digitalocean_droplet" "worker-1" {
 
   provisioner "remote-exec" {
     inline = [
-      "chmod 600 /root/.ssh/id_rsa", 
+      "chmod 600 /root/.ssh/id_rsa",
+      "sudo ufw allow 2377/tcp",
+      "sudo ufw allow 7946/tcp",
+      "sudo ufw allow 7946/udp",
+      "sudo ufw allow 4789/udp",
+      "sudo ufw reload",
       "mkdir -p /tmp",
-      "scp -o StrictHostKeyChecking=no -o BatchMode=yes -i /root/.ssh/id_rsa root@${data.digitalocean_droplet.manager.ipv4_address}:/tmp/swarm_token /tmp",
-      "docker swarm join --token \"$(cat /tmp/swarm_token)\" ${data.digitalocean_droplet.manager.ipv4_address_private}:2377",
-    ]
-  }
-}
-
-resource "digitalocean_droplet" "worker-2" {
-  image    = "docker-20-04"
-  name     = "worker-2"
-  region   = "fra1"
-  size     = "s-1vcpu-1gb"
-  ssh_keys = [data.digitalocean_ssh_key.Viktoria.id]
-
-  connection {
-    type        = "ssh"
-    user        = "root"
-    private_key = file(var.pvt_key)
-    host        = self.ipv4_address
-  }
-
-  provisioner "file" {
-    source      = var.pvt_key
-    destination = "/root/.ssh/id_rsa"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "chmod 600 /root/.ssh/id_rsa", 
-      "mkdir -p /tmp",
-      "scp -o StrictHostKeyChecking=no -o BatchMode=yes -i /root/.ssh/id_rsa root@${data.digitalocean_droplet.manager.ipv4_address}:/tmp/swarm_token /tmp",
-      "docker swarm join --token \"$(cat /tmp/swarm_token)\" ${data.digitalocean_droplet.manager.ipv4_address_private}:2377",
+      "scp -o StrictHostKeyChecking=no -o BatchMode=yes -i /root/.ssh/id_rsa root@${local.manager_ip}:/tmp/swarm_token /tmp",
+      "SWARM_TOKEN=$(cat /tmp/swarm_token)",
+      "docker swarm join --token $SWARM_TOKEN ${local.manager_ip}:2377",
     ]
   }
 }
